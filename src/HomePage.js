6f745c3b12dcf763872258b1a56e4601d930234e
Harsh Soni <107264237+Harsh200112@users.noreply.github.com>
2023-11-21 17:41:14 +0530

    commit 1

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,27 +1,29 @@
 import React, {useEffect} from "react";
 import {useState} from "react";
 import CardSuggestions from "./CardSuggestions";
-import {queries} from "@testing-library/react";
-
+import { fetchVideoIds } from "./api/search";
+import { debounce } from 'lodash';
 
 export default function HomePage() {
-        const [inputText, setInputText] = useState("");
-        const [videos, setvideos] = useState([]);
-        const getVideos = async () => {
-            try {
-                let imageURLs = [];
-                for (let i = 0; i < 10; i++) {
-                    const res = await fetch('https://random.imagecdn.app/500/150');
-                    const imageBlob = await res.blob();
-                    imageURLs[i] = URL.createObjectURL(imageBlob);
-                }
-                setvideos(imageURLs);
-                console.log(videos);
-
-            } catch (error) {
-                console.error("Error fetching videos:", error);
+    const [inputText, setInputText] = useState("");
+    var [videoIds, setVideoIds] = useState([]);
+    
+    const [videos, setvideos] = useState([]);
+    const getVideos = async () => {
+        try {
+            let imageURLs = [];
+            for (let i = 0; i < 10; i++) {
+                const res = await fetch('https://random.imagecdn.app/500/150');
+                const imageBlob = await res.blob();
+                imageURLs[i] = URL.createObjectURL(imageBlob);
             }
-        };
+            setvideos(imageURLs);
+            console.log(videos);
+
+        } catch (error) {
+            console.error("Error fetching videos:", error);
+        }
+    };
     useEffect(() => {
         // Use an IIFE (Immediately Invoked Function Expression) to call async function
         ( async () => {
@@ -30,17 +32,31 @@ export default function HomePage() {
         })();
     }, []);
 
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await fetchVideoIds(inputText);
+            setVideoIds(data);
+            console.log(inputText, "text received");
+            console.log(videoIds);
+        };
+    
+        fetchData();
+    }, [inputText]);
+
     const handleChange = () => {
         var inputValue = document.getElementById("523").value;
+        setVideoIds([]);
         setInputText(inputValue);
-        console.log(inputText);
     };
 
-    const sendInput = () => {
+    const sendInput = async () => {
+        const data = await fetchVideoIds(inputText);
+        setVideoIds(data);
         console.log(inputText, "text received");
-    }
+    };
+
         return (
-            <div className={"h-screen w-screen justify-evenly flex-col border-4 "}>
+            <div className={"h-screen w-screen justify-evenly flex-col"}>
                 <div className={"h-1/5 w-full flex  bg-amber-300 justify-center  items-center"}>
                     <input onChange={handleChange} id={"523"} value={inputText} className={"h-12 border-none w-5/12 rounded-3xl p-4"  }></input>
                     <button onClick={sendInput} className={"h-12 border-4 border-none w-3/12 rounded-3xl text-center text-white bg-black mx-7 "}>Search
